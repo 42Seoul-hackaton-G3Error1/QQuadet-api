6970ff1600a8867f33d04ae99a078a3819513c09
jaeskim <jaeskim@student.42seoul.kr>
2021-01-11 17:06:47 +0900

    fix openapi location

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@
 /*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 16:59:58 by jaeskim           #+#    #+#             */
-/*   Updated: 2021/01/11 17:00:05 by jaeskim          ###   ########.fr       */
+/*   Updated: 2021/01/11 17:05:15 by jaeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
 const IndexPage = () => {
-  return <SwaggerUI url={"/docs.yml"} />;
+  return <SwaggerUI url={"/docs.json"} />;
 };
 
 export default IndexPage;
